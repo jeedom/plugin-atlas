a5b7592ce481c7a5d39b99ff29c8cbfc1cb5a917
Rocket13011 <alexandre@jeedom.com>
2021-08-24 11:31:13 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -19,17 +19,17 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 // Fonction exécutée automatiquement après l'installation du plugin
-  function template_install() {
+  function atlas_install() {
 
   }
 
 // Fonction exécutée automatiquement après la mise à jour du plugin
-  function template_update() {
+  function atlas_update() {
 
   }
 
 // Fonction exécutée automatiquement après la suppression du plugin
-  function template_remove() {
+  function atlas_remove() {
 
   }
 
